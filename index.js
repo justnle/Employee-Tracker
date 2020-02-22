123a41dcaf425a3ad0f01c25ec8d64f6f6ac64da
Justin <twoplatinumchainz@gmail.com>
2020-02-21 18:24:50 -0800

    viewEmployees() and addEmployees() working

--- index.js
+++ index.js
@@ -36,15 +36,17 @@ const roleChoices = [
   'Lawyer'
 ];
 
+// HAS TO DYNAMICALLY UPDATE or PULL FROM THE DB
 const employeeChoices = [
-    'John Doe',
-    'Mike Chan',
-    'Ashley Rodriguez',
-    'Kevin Tupik',
-    'Malia Brown',
-    'Sarah Lourd',
-    'Tom Allen',
-    'Christian Eckenrode'
+  'John Doe',
+  'Mike Chan',
+  'Ashley Rodriguez',
+  'Kevin Tupik',
+  'Malia Brown',
+  'Sarah Lourd',
+  'Tom Allen',
+  'Christian Eckenrode',
+  'None'
 ];
 
 const connection = mysql.createConnection({
@@ -78,7 +80,7 @@ function prompt() {
       ]
     })
     .then(answer => {
-    //   console.log('answer', answer);
+      //   console.log('answer', answer);
       switch (answer.action) {
         case promptMessages.viewEmployees:
           viewEmployees();
@@ -115,45 +117,48 @@ function prompt() {
     });
 }
 
-// WIP
+/*
+TODO first_name, last_name FROM employees WHERE manager_id = id -- MANAGER
+*/
 function viewEmployees() {
-    const query = 'SELECT id, first_name, last_name FROM employees';
-    connection.query(query, (err, res) => {
-        if (err) throw err;
-        const employeeTable = cTable.getTable(res);
-        console.log('\n' + employeeTable);
-        prompt();
-    });
-    
-    /*
-     TODO JOIN: title FROM role WHERE employee.role_id = role.id -- ROLE TITLE
-     TODO JOIN: name FROM department WHERE role.department_id = department.id  -- DEPARTMENT
-     TODO JOIN: salary FROM role WHERE role.id = employees.role_id -- SALARY
-     TODO JOIN: first_name, last_name FROM employees WHERE manager_id = id -- MANAGER
-    */
+  const query = `
+    SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name, role.salary, employee.manager_id
+    FROM employee 
+        INNER JOIN role 
+            ON (employee.role_id = role.id)
+        INNER JOIN department
+            ON (role.department_id = department.id)
+    ORDER BY employee.id
+    `;
+  connection.query(query, (err, res) => {
+    if (err) throw err;
+    const employeeTable = cTable.getTable(res);
+    console.log('\n' + employeeTable);
+    prompt();
+  });
 }
 
 function viewDepartments() {
-    const query = 'SELECT name FROM department';
-    connection.query(query, (err, res) => {
-        if (err) throw err;
-        const departmentTable = cTable.getTable(res);
-        console.log('\n' + departmentTable);
-        prompt();
-    });
+  const query = 'SELECT name FROM department';
+  connection.query(query, (err, res) => {
+    if (err) throw err;
+    const departmentTable = cTable.getTable(res);
+    console.log('\n' + departmentTable);
+    prompt();
+  });
 }
 
 function viewRoles() {
-    const query = 'SELECT title FROM role';
-    connection.query(query, (err, res) => {
-        if (err) throw err;
-        const roleTable = cTable.getTable(res);
-        console.log('\n' + roleTable);
-        prompt();
-    });
+  const query = 'SELECT title FROM role';
+  connection.query(query, (err, res) => {
+    if (err) throw err;
+    const roleTable = cTable.getTable(res);
+    console.log('\n' + roleTable);
+    prompt();
+  });
 }
 
-// WIP
+// DRY OUT
 function addEmployee() {
   inquirer
     .prompt([
@@ -181,7 +186,73 @@ function addEmployee() {
       }
     ])
     .then(answer => {
-    //   console.log('answer', answer);
+      switch (answer.role) {
+        case 'Sales Lead':
+          answer.role = 1;
+          break;
+        case 'Salesperson':
+          answer.role = 2;
+          break;
+        case 'Lead Engineer':
+          answer.role = 3;
+          break;
+        case 'Software Engineer':
+          answer.role = 4;
+          break;
+        case 'Accountant':
+          answer.role = 5;
+          break;
+        case 'Legal Team Lead':
+          answer.role = 6;
+          break;
+        case 'Lawyer':
+          answer.role = 7;
+          break;
+      }
+
+      switch (answer.manager) {
+        case 'John Doe':
+          answer.manager = 1;
+          break;
+        case 'Mike Chan':
+          answer.manager = 2;
+          break;
+        case 'Ashley Rodriguez':
+          answer.manager = 3;
+          break;
+        case 'Kevin Tupik':
+          answer.manager = 4;
+          break;
+        case 'Malia Brown':
+          answer.manager = 5;
+          break;
+        case 'Sarah Lourd':
+          answer.manager = 6;
+          break;
+        case 'Tom Allen':
+          answer.manager = 7;
+          break;
+        case 'Christian Eckenrode':
+          answer.manager = 8;
+          break;
+        default:
+          answer.manager = null;
+      }
+
+      const query = connection.query(
+        'INSERT INTO employee SET ?',
+        {
+          first_name: answer.firstName,
+          last_name: answer.lastName,
+          role_id: answer.role,
+          manager_id: answer.manager
+        },
+        (err, res) => {
+          if (err) throw err;
+          // console.log(res);
+          prompt();
+        }
+      );
     });
 }
 
