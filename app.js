6f4c193e159bbd3822c7d780c512b820b1198c4d
Justin <twoplatinumchainz@gmail.com>
2020-03-01 10:14:42 -0800

    eslint

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 'use strict';
 
-const mysql = require('mysql');
-const { initApp } = require('./lib/appFunctions');
-const { initPrompts } = require('./lib/prompts');
+const mysql = require(`mysql`);
+const { initApp } = require(`./lib/appFunctions`);
+const { initPrompts } = require(`./lib/prompts`);
 
 const connection = mysql.createConnection({
-  host: 'localhost',
+  host: `localhost`,
   port: 3306,
-  user: 'root',
-  password: 'testing123',
-  database: 'employees_db'
+  user: `root`,
+  password: `testing123`,
+  database: `employees_db`
 });
 
 connection.connect(err => {
-  if (err) throw err;
+  if (err) { throw err; }
   initPrompts();
   initApp();
 });
